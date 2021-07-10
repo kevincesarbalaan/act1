72faa450a9a0f3a26d3e58cdea0254d884d23042
kevincesarbalaan <60724351+kevincesarbalaan@users.noreply.github.com>
2021-07-10 17:07:05 +0800

    Add files via upload

---
+++ storage/framework/views/9b5d12ab4bb5254690f156fd0469b5480c3e8d42.php
@@ -0,0 +1,121 @@
+<Doctype html>
+<html>
+<head>
+    <style>body{
+    margin: auto;
+    background: #efefef;
+    font-family: arial;
+}
+.nav_bar{
+    margin: auto;
+    border-bottom: 1px solid #000000;
+    width: 1350px;
+    padding: 0px 43px 0px 43px;
+    height: 64px;
+    margin-top: 30px;
+}
+.nav_bar ul{
+    padding: 0;
+    list-style: none;
+}
+.nav_bar ul li{
+    float: left;
+    font-size: 16px;
+    font-weight: bold;
+    margin-right: 10px;
+}
+.nav_bar ul li a{
+    text-decoration: none;
+    color: #000000;
+    background: #6db1e4;
+    border: 1px solid #000000;
+    border-bottom: none;
+    padding: 23px 20px 22px 20px;
+    -moz-border-radius-topleft: 10px;
+    -moz-borderradius-topright: 10px;
+    width: 90px;
+    display: block;
+    text-align: center;
+}
+.nav_bar ul li a:hover{
+    text-decoration: none;
+    color: #000000;
+    background: #96e0e9;
+    -moz-transition: background-color 200ms ease-in;
+    -webkit-transition: background-color 200ms ease-in;
+    -ms-transition:background-color 200ms ease-in;
+    -o-transition: background-color 200ms ease-in;
+    transition: background-color 200ms ease-in;
+}
+.nav_bar ul li a#onlink{
+    background: #ffffff;
+    color: #000000;
+    border-bottom: 1px solid #ffffff;
+}
+.nav_bar ul lu a#onlink:hover{
+    background: #ffffff;
+    color: #000000;
+}
+.main_container{
+    margin: auto;
+    width: 1075px;
+    padding: 100px;
+    border: 1px solid #000000;
+    min-height: 500px;
+    border-top: none;
+    background: 17.jpeg;
+}
+.main_container p{
+    font-size: 20px;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}</style>
+    <title>Laravel</title>
+    <link herf="/public/css/sytle.css" rel="sytlesheet">
+    <style>
+        body{
+            background-color: aquamarine;
+            text-align: center;
+            
+        }
+        img{
+            width: 700px;
+        }
+    </style>
+</head>    
+<body>
+    <br>
+    <h1>Ilocos Norte Tourist Spot</h1>
+    <div class="nav_bar">
+    </div>
+    <div class="main_container">
+        <img src="https://github.com/kevincesarbalaan/act1/blob/main/3.jpg?raw=true">
+        <p>Marvel at the Majestic Paoay Church</p>
+        <p>Paoay Church also known as San Agustin Church in Ilocos Norte
+The Paoay Church, a UNESCO World Heritage-listed site, is worth a visit when you go up north. The church, also known as the San Agustin Church, is about 19km southwest of Laoag. 
+ </p>
+ <img src="https://github.com/kevincesarbalaan/act1/blob/main/1.jpg?raw=true" >
+ <p>Be in Awe by the Bangui Windmills</p>
+    <p>Bangui Windmills in Pagudpud, Ilocos Norte
+The Bangui Windmills are a long line of 70m-high wind turbines lined up over a 9km stretch of the Bangui Bay shoreline.
+<p>Standing tall over the black sand beach, the windmills make up the first wind farm in the country and generate enough electricity to support 40% of the power requirements of Ilocos Norte.
+ </p>
+  <img src="https://github.com/kevincesarbalaan/act1/blob/main/5.jpg?raw=true">
+        <p>Go Sandboarding at the La Paz and Paoay Sand Dunes
+People during atv ride in Paoay Sand Dunes</p>
+        <p>The sand dunes of La Paz and Paoay, the natural coastal desert shaped by sea and wind, are located along the coast near Laoag City. Its rolling and sometimes steep slopes make it a perfect place for a heart-pumping 4x4 ride and sandboarding.</p>
+        <img src="https://github.com/kevincesarbalaan/act1/blob/main/4.jpg?raw=true">
+        <p>Discover Kapurpurawan Rock Formation
+Kapurpurawan Rock Formation in Burgos, Ilocos Norte
+</p>
+        <p>Many tourists go to Laoag for the sand dunes, or to nearby Pagudpud to swim in its beaches. But in the nearby town of Burgos, a hidden gem is waiting to be discovered: the Kapurpurawan Rock Formation.</p>
+        <p>It is a natural formation that juts out of the rock along the beach. Its name was derived from the Ilocano word “puraw” meaning “white.”</p>
+        <img src="https://github.com/kevincesarbalaan/act1/blob/main/2.jpg?raw=true">
+        <p>See the Sinking Bell Tower</p>
+        <p>Sinking Bell Tower in Laoag, Ilocos Norte
+Along a busy road about 85 meters away from the Laoag Church is the Sinking Bell Tower. The bell tower was built in the 1600s by the Augustinian friars, who did not immediately realize that it was on sandy soil.
+</p>
+    </div>
+</body>    
+</html><?php /**PATH C:\Users\KC\act1-app\resources\views/home.blade.php ENDPATH**/ ?>
